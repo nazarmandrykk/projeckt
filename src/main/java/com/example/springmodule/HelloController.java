914fd3cea5958fc8f9a6de41f151bf3ab1b459e1
NazarMandryk <nazarmandryk4@gmail.com>
2023-03-09 21:43:53 +0200

    comitForYura

--- src/main/java/com/example/springmodule/HelloController.java
+++ src/main/java/com/example/springmodule/HelloController.java
@@ -1,16 +1,17 @@
 package com.example.springmodule;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
 import x1.lib1;
 
 import java.util.ArrayList;
 
 @RestController
 public class HelloController {
-
+    String nameall;
     @GetMapping("/greeting")
     public String greeting(@RequestParam("name") String name) {
         ArrayList<String> strings = new ArrayList<>();
@@ -20,19 +21,57 @@ public class HelloController {
         strings.add("Andea");
 
 
-        if (!strings.contains(name)) {
-            return "Імя немає у списку ";
+        if (!isValidNameFormat(name)) {
+            throw new InvalidNameFormatException();
         }
 
+        if (!strings.contains(name)) {
+            return "Ім'я не знайдено у списку";
+        }
 
-        ArrayList charName = new ArrayList<>();
+        ArrayList<Character> charName = new ArrayList<>();
+        for (char c : x1.lib1.charSearchInString(name)) {
+            charName.add(c);
+        }
 
-        if(name.equals("Nazar")) {
-            for (char c : x1.lib1.charSearchInString(name)) {
-                charName.add(c);
-            }
-        }else return "Це не наше імя";
-
-        return "Привіт, " + name + " char :  " + charName.toString() ;
+        return "Привіт, " + name + "! Символи в імені: " + charName;
     }
+
+
+    private boolean isValidNameFormat(String name) {
+        char firstChar = name.charAt(0);
+        if (!Character.isUpperCase(firstChar)) {
+            return false;
+        }
+        for (int i = 1; i < name.length(); i++) {
+            char c = name.charAt(i);
+            if (Character.isUpperCase(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @ControllerAdvice
+    public class NameFormatAdvice {
+
+        @ExceptionHandler(InvalidNameFormatException.class)
+        public String handleInvalidNameFormat() {
+            return "error";
+        }
+    }
+    @GetMapping("/error")
+    public String errPage(){
+        return "error";
+    }
+
+
+
+    // Виняток для помилок у форматуванні імені
+    public class InvalidNameFormatException extends RuntimeException {
+    }
+
 }
+
+
+
---
+++ src/main/resources/templates/error.ftl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<#assign title = "Error">
+<title>${title}</title>
+</head>
+<body>
+<h1>${title}</h1>
+<p>${errorMessage}</p>
+</body>
+</html>
